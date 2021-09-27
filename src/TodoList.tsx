f89be0d7bb26cd0d4e8116d1283ab6a508665525
Farrant <pcsh810778@gmail.com>
2021-09-27 08:00:43 +0800

    feat: change todo data structure

--- src/TodoList.tsx
+++ src/TodoList.tsx
@@ -1,26 +1,24 @@
 import React, { useState } from "react"
 
-type TodoGroup = {
-  id: string
-  value?: String
-  list: Todo[]
-}
-
 type Todo = {
   id: string
   value: string
   isDone: boolean
+  list?: Todo[]
 }
 
 function TodoList() {
-  const [list, setList] = useState<TodoGroup[]>([
+  const [list, setList] = useState<Todo[]>([
     {
       id: "1",
+      value: "",
+      isDone: false,
       list: [{ id: "a", value: "todo-01", isDone: false }],
     },
     {
       id: "2",
       value: "第一個群組",
+      isDone: false,
       list: [
         {
           id: "a",
@@ -37,6 +35,7 @@ function TodoList() {
     {
       id: "3",
       value: "第二個群組",
+      isDone: false,
       list: [
         {
           id: "a",
@@ -53,125 +52,102 @@ function TodoList() {
   ])
 
   const handleAddTodoGroupItem = () => {
-    setList((prevList) => [
-      ...prevList,
-      {
-        id: "2",
-        value: "第一個群組",
-        list: [
-          {
-            id: "a",
-            value: "group-todo-01",
-            isDone: false,
-          },
-        ],
-      },
-    ])
+    // setList((prevList) => [
+    //   ...prevList,
+    //   {
+    //     id: "2",
+    //     value: "第一個群組",
+    //     list: [
+    //       {
+    //         id: "a",
+    //         value: "group-todo-01",
+    //         isDone: false,
+    //       },
+    //     ],
+    //   },
+    // ])
   }
   const handleAddTodoItem = () => {
-    setList((prevList) => [
-      ...prevList,
-      {
-        id: "2",
-        list: [
-          {
-            id: "a",
-            value: "group-todo-01",
-            isDone: false,
-          },
-        ],
-      },
-    ])
+    // setList((prevList) => [
+    //   ...prevList,
+    //   {
+    //     id: "2",
+    //     list: [
+    //       {
+    //         id: "a",
+    //         value: "group-todo-01",
+    //         isDone: false,
+    //       },
+    //     ],
+    //   },
+    // ])
   }
-  const handleCheckTodoItem = (itemId: string, subItemId: string) => {
-    setList((prevList) => {
-      const newPrevList = [...prevList]
-      const currentIndex = newPrevList.findIndex((item) => (item.id = itemId))
-      const currentSubIndex = newPrevList[currentIndex].list.findIndex(
-        (subItem) => {
-          console.log((subItem.id = subItemId))
-          return (subItem.id = subItemId)
-        }
-      )
-
-      newPrevList[currentIndex].list[currentSubIndex].isDone =
-        !newPrevList[currentIndex].list[currentSubIndex].isDone
-
-      return newPrevList
-    })
-  }
+  const handleCheckTodoItem = (itemId: string, subItemId: string) => {}
   const handleEditTodoItem = (itemId: string, subItemid: string) => {}
-  const handleDeleteTodoItem = (itemId: string, subItemId: string) => {
-    setList((prevList) => {
-      const currentIndex = prevList.findIndex((item) => item.id === itemId)
-      const newItem =
-        prevList[currentIndex].list.filter(
-          (subItem) => subItem.id !== subItemId
-        ).length === 0
-          ? []
-          : [
-              {
-                ...prevList[currentIndex],
-                list: prevList[currentIndex].list.filter(
-                  (subItem) => subItem.id !== subItemId
-                ),
-              },
-            ]
+  const handleDeleteTodoItem = (itemId: string, subItemId: string) => {}
 
-      return [
-        ...prevList.slice(0, currentIndex),
-        ...newItem,
-        ...prevList.slice(currentIndex + 1),
-      ]
-    })
-  }
+  const a = typeof list
 
   return (
     <div>
       {list.map((item) => (
         <div key={item.id}>
-          {item.value ? (
-            <>
-              <h2>{item.value}</h2>
-              <ul>
-                {item.list.map((todo) => (
-                  <li key={todo.id}>
-                    <input type="text" value={todo.value} />
-                    <input
-                      type="checkbox"
-                      checked={todo.isDone}
-                      onChange={() => handleCheckTodoItem(item.id, todo.id)}
-                    />
-                    <button
-                      onClick={() => handleDeleteTodoItem(item.id, todo.id)}
-                    >
-                      delete
-                    </button>
-                  </li>
-                ))}
-              </ul>
-            </>
-          ) : (
-            item.list.map((todo) => (
-              <li key={todo.id}>
-                <input type="text" value={todo.value} />
-                <input
-                  type="checkbox"
-                  checked={todo.isDone}
-                  onChange={() => handleCheckTodoItem(item.id, todo.id)}
+          {item ? (
+            <TodoGroup groupName={item.value} list={item.list} />
+          ) : typeof item.value === "undefined" ? (
+            <ul>
+              {item.list.map((todo) => (
+                <Todo
+                  key={todo.id}
+                  id={todo.id}
+                  value={todo.value}
+                  isDone={todo.isDone}
                 />
-                <button onClick={() => handleDeleteTodoItem(item.id, todo.id)}>
-                  delete
-                </button>
-              </li>
-            ))
-          )}
+              ))}
+            </ul>
+          ) : null}
         </div>
       ))}
 
       <button onClick={handleAddTodoGroupItem}>新增 Todo Group</button>
       <button onClick={handleAddTodoItem}>新增 Todo</button>
     </div>
+  )
+}
+
+function TodoGroup(props: { groupName: string; list: Todo[] }) {
+  const { groupName, list } = props
+  return (
+    <>
+      <h2>{groupName}</h2>
+      <ul>
+        {list.map((todo) => (
+          <Todo
+            key={todo.id}
+            id={todo.id}
+            value={todo.value}
+            isDone={todo.isDone}
+          />
+        ))}
+      </ul>
+    </>
+  )
+}
+
+function Todo(props: Todo) {
+  const { value, isDone } = props
+  return (
+    <li>
+      <input type="text" value={value} />
+      <input
+        type="checkbox"
+        checked={isDone}
+        onChange={() => handleCheckTodoItem(item.id, todo.id)}
+      />
+      <button onClick={() => handleDeleteTodoItem(item.id, todo.id)}>
+        delete
+      </button>
+    </li>
   )
 }
 
